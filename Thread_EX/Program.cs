9dee625fde7a830576d53170f5f378221ebc9496
남정욱 <wjddnr3304@gmail.com>
2024-10-15 10:35:18 +0900

    스레드 메서드 예제

--- Thread_EX/Program.cs
+++ Thread_EX/Program.cs
@@ -4,14 +4,18 @@
     {
         static void ThreadProc()
         {
-            Console.WriteLine($"스레드 ID{Thread.CurrentThread.GetHashCode()}");
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.Write($"{i}");
+                Thread.Sleep( 200 );
+            }
         }
         static void Main(string[] args)
         {
-            Thread thread = new Thread(ThreadProc);
-
+            Thread thread = new Thread(new ThreadStart(ThreadProc));
             thread.Start();
-            Console.WriteLine($"메인 스레드 ID{Thread.CurrentThread.GetHashCode()}");
+            thread.Join(); // 위에 부 스레드가 끝날때 까지 메인종료 메세지( 주 스레드 ) 대기시킴
+            Console.WriteLine("메인 종료");
         }
     }
 }
